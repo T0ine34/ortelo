c23b84e76621127b6717c588c8f1f514de8f1e19
T0ine34 <91411073+T0ine34@users.noreply.github.com>
2023-10-16 09:14:13 +0200

    adding comments in server and client scripts

--- public/client.js
+++ public/client.js
@@ -10,7 +10,7 @@ myForm.innerHTML = '<input id="usernameInput" autocomplete="on" placeholder="Cho
 mydiv.appendChild(myForm)
 document.body.appendChild(mydiv);
 
-myForm.addEventListener('submit', function(e){
+myForm.addEventListener('submit', function(e){                      //this is triggered when the user click on "CHOISIR"
     e.preventDefault()
     let uin = document.querySelector("#usernameInput")
     if(!uin.value) return;
@@ -18,9 +18,9 @@ myForm.addEventListener('submit', function(e){
     socket = io();
 
     let username = uin.value
-    socket.emit('newUser', uin.value)
+    socket.emit('newUser', username);                               //sending the newUser event to the server, with the username as parameter
 
-    socket.on('newUser', (name) => {
+    socket.on('newUser', (name) => {                                //catching the newUser event, triggered by the server when a new user joins the chat
         let item = document.createElement('li');
         item.textContent = name + " a rejoint le chat ! ";
         item.innerHTML += "&#128075;"
@@ -28,7 +28,7 @@ myForm.addEventListener('submit', function(e){
         window.scrollTo(0, document.body.scrollHeight);
     })
 
-    socket.on('new_message', (username, msg) => {
+    socket.on('new_message', (username, msg) => {                   //catching the new_message event, triggered by the server when a user sends a message
         receive_message(username, msg)
     })
 
@@ -36,10 +36,10 @@ myForm.addEventListener('submit', function(e){
     let messages = document.querySelector('#messages');
     let input = document.querySelector('#input');
 
-    form.addEventListener('submit', function(e) {
+    form.addEventListener('submit', function(e) {                   //this is triggered when the user click on "Send"
         e.preventDefault();
         if (input.value) {
-            socket.emit('send_message', username, input.value);
+            socket.emit('send_message', username, input.value);     //sending the send_message event to the server, with the username and the message as parameters
             input.value = '';
         }
     });
--- server.js
+++ server.js
@@ -11,19 +11,19 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-
-    socket.on('newUser', (name) => {
-        io.emit("newUser", name);
+io.on('connection', (socket) => {                       //catching the connection of a new user
+                                                        //socket is the link between this user and the server
+    socket.on('newUser', (name) => {                    //catching the newUser event, triggered by the client when he click on "CHOISIR"
+        io.emit("newUser", name);                       //broadcasting the newUser event to all the users, including the new one
     })
 
 
-    socket.on('disconnect', () => {
-
+    socket.on('disconnect', () => {                     //catching the disconnect event, triggered by the client when he leaves the chat
+                                                        //NOT IMPLEMENTED YET
     })
 
-    socket.on('send_message', (username, msg) => {
-        io.emit('new_message', username, msg)
+    socket.on('send_message', (username, msg) => {      //catching the send_message event, triggered by the client when he sends a message
+        io.emit('new_message', username, msg)           //broadcasting the new_message event to all the users, including the sender
     })
 
 
