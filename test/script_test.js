d47023698ebc20d4327ad2429e62d834e58d8280
Boufrour Jebril <jebril.boufrour@etu.univ-amu.fr>
2023-10-19 09:48:07 +0200

    Add user simulation test for chat testing

--- test/script_test.js
+++ test/script_test.js
@@ -1,24 +1,29 @@
 const io = require('socket.io-client');
-const SERVER_URL = '192.168.76.59:3000'; //l'URL du serveur
+const SERVER_URL = 'http://localhost:3000';
+//const SERVER_URL = '192.168.76.59:3000'; //L'URL du serveur
 
-const USER_COUNT = 10; // Nombre d'utilisateurs à simuler
-const MESSAGES_TO_SEND = 5; // Nombre de messages que chaque utilisateur enverra
+const USER_COUNT = 5; // Nombre d'utilisateurs à simuler
+const MESSAGES_TO_SEND = 5;
+
+const simulateUser = (userId) => {
+    const socket = io(SERVER_URL);
+    const username = `Lila_${userId}`;
+
+    socket.on('connect', () => {
+        console.log(`Utilisateur ${username} est en train de se connecter.`);
+
+        // Simuler le choix d'un pseudo
+        socket.emit('newUser', username);
+
+        socket.on('newUser', (name) => {
+            console.log(`Notification: ${name} a rejoint le chat!`);
+        });
+
+        
+    });
+};
 
 for (let i = 0; i < USER_COUNT; i++) {
-  // Dimulation d'un utilisateur qui se connecte
-  const socket = io(SERVER_URL);
-
-  socket.on('connect', () => {
-    console.log(`Utilisateur ${i} connecté avec l'ID ${socket.id}`);
-
-    for (let j = 0; j < MESSAGES_TO_SEND; j++) {
-      // Envoyer un message
-      socket.emit('sendMessage', `Utilisateur ${i}: Message ${j}`);
-    }
-  });
-
-  // Écouter les messages reçus et les afficher
-  socket.on('receiveMessage', (message) => {
-    console.log(`Utilisateur ${i} a reçu: ${message}`);
-  });
+    simulateUser(i);
 }
+
