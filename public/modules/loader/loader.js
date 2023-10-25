f417278f5b2d1759b31e9d9abfbd2a31b3a020f7
Lila BRANDON <mimifan.dev@gmail.com>
2023-10-25 15:45:10 +0200

    adds class game

--- public/modules/loader/loader.js
+++ public/modules/loader/loader.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const JSZip = require('jszip');
-
+const { Game } = require('./game');
 
 class GameLoader {
     
@@ -10,37 +10,23 @@ class GameLoader {
         this._gameFiles = []
     }
 
-    async getFiles() {
-        fs.readdir(this._path, (err, data) => {
-            if(err) throw err
-            for(let i = 0; i < data.length; i++){
-                this._gameFiles.push(`${this._path}${data[i]}`)
-            }
-        })
+    getFiles() {
+        let data = fs.readdirSync(this._path);
+        for(let i = 0; i < data.length; i++){
+            this._gameFiles.push(`${this._path}${data[i]}`)
+        }
     }
 
 
     readAGame(filePath) {
-        fs.readFile(filePath, (err, data) => {
-            if (err) throw err;
-
-            this._jsZip.loadAsync(data).then(function (zipFile) {
-                console.log(zipFile);
-            });
-
-        });
+        (new Game(filePath)).init()
     }
 
     addGameToApp(filePath){
-        fs.readFile(filePath, (err, data) => {
-            if (err) throw err;
 
-            this._jsZip.loadAsync(data).then(function (zipFile) {
-
-                // TODO CREATE HTML ELEMENTS
-
-            });
-
+        let data = fs.readFileSync(filePath);
+        this._jsZip.loadAsync(data).then(function (zipFile) {
+            console.log(zipFile);
         });
     }
 
