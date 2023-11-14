a72a5ede483bd1284f15ad3b3820544d5af6d820
T0ine34 <91411073+T0ine34@users.noreply.github.com>
2023-11-14 14:06:01 +0100

    removing unused code in server.js

--- server.js
+++ server.js
@@ -60,21 +60,6 @@ async function loadGames() {
     // gameLoader.readAGame()
     //TODO GET ALL GAMES TO SHOW ON WEBPAGE
 }
-    //                                                     //socket is the link between this user and the server
-    // socket.on('newUser', (name) => {                    //catching the newUser event, triggered by the client when he click on "CHOISIR"
-    //     io.emit("newUser", name);                       //broadcasting the newUser event to all the users, including the new one
-    // })
-
-
-    // socket.on('disconnect', (reason) => {                     //catching the disconnect event, triggered by the client when he leaves the chat
-    //     io.emit("new_message", "SERVER", "Un utilisateur a quitté le chat");
-    //     io.emit("new_message", "SERVER", "Raison : " + reason);
-    // })
-
-    // socket.on('send_message', (username, msg) => {      //catching the send_message event, triggered by the client when he sends a message
-    //     io.emit('new_message', username, msg)           //broadcasting the new_message event to all the users, including the sender
-    // })
-
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
