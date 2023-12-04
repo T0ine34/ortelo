a696042deb9b5c4e595835afde4bdd7abd08eb6f
Diez Tom <tom.diez@etu.univ-amu.fr>
2023-12-04 13:40:20 +0100

    game

--- server_modules/loader/game.js
+++ server_modules/loader/game.js
@@ -5,6 +5,7 @@ class Game {
     constructor(gameFiles, gameName) {
         this._gameFiles = gameFiles;
         this._name = gameName;
+        this._starterFunction = null;
         // Initialisation des variables pour stocker les contenus des fichiers
         this._iconData = null;
         this._htmlData = null;
@@ -16,14 +17,14 @@ class Game {
     async init() {
         try {
             const gameFolderPrefix = this._name + '/';
-            const indexPath = gameFolderPrefix + 'index.json';
-
+            const indexPath =  gameFolderPrefix + 'index.json';
             if (!this._gameFiles[indexPath]) {
                 throw new Error(indexPath + ' not found in game files');
             }
 
             const jsonData = JSON.parse(this._gameFiles[indexPath].toString('utf8'));
             this._name = jsonData.name || "Unknown Name";
+            this._starterFunction = gameFolderPrefix + jsonData.starterfunction;
 
             // Stocker le contenu des fichiers en mémoire
             this._htmlData = this._gameFiles[gameFolderPrefix + jsonData.html]?.toString('utf8');
@@ -32,7 +33,7 @@ class Game {
             this._serverData = this._gameFiles[gameFolderPrefix + jsonData.server]?.toString('utf8');
 
             // Gérer l'icône
-            let iconPath = gameFolderPrefix + 'images/' + jsonData.images.icon;
+            let iconPath =  gameFolderPrefix + 'images/' + jsonData.images.icon;
             if (this._gameFiles[iconPath]) {
                 this._iconData = this._gameFiles[iconPath];
             } else {
@@ -46,6 +47,9 @@ class Game {
     // Getters pour accéder aux données des fichiers
     get name() {
         return this._name;
+    }
+    get starterFunction() {
+        return this._starterFunction;
     }
 
     get iconData() {
