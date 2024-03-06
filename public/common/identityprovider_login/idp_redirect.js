ba154cb2461cbb42659f8d7df35547920f1910d9
Lila BRANDON <mimifan.dev@gmail.com>
2024-03-06 19:42:47 +0100

    Replaced localhost:3000  by /

--- public/common/identityprovider_login/idp_redirect.js
+++ public/common/identityprovider_login/idp_redirect.js
@@ -8,7 +8,7 @@ emailjs.init("Oy9a9uSnZvDAnliA0");
 const config = {
     authority: 'https://accounts.google.com',
     client_id: '51873909339-6n41as7geb9le4cg77m3l18e88pv51j7.apps.googleusercontent.com',
-    redirect_uri: 'http://localhost:3000/identityprovider_login/oidcredirect.html',
+    redirect_uri: '/identityprovider_login/oidcredirect.html',
     response_type: 'id_token token',
     scope: 'openid profile email',
 };
