e2fba45da3b1843548f42ceea841513e791abf42
Antoine <91411073+t0ine34@users.noreply.github.com>
2024-03-19 18:44:38 +0100

    moving progressBar class in a dedicated file

--- building/game-assemble.py
+++ building/game-assemble.py
@@ -4,39 +4,11 @@ from zipfile import ZipFile
 
 from config import Config
 
+from progressBar import ProgressBar
+
 PARENT_FOLDER = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-class ProgressBar:
-    def __init__(self, total = 100, width = 50):
-        self.total = total
-        self.current = 0
-        self.last = 0
-        self.width = width
-        
-    def update(self, current):
-        self.current = current
-        if self.current > self.total:
-            self.current = self.total
-        if self.current != self.last:
-            self.last = self.current
-            self.draw()
-            
-    def draw(self):
-        percent = self.current / self.total
-        filled = int(percent * self.width)
-        print("\r[%s%s] %d%%" % ("#" * filled, " " * (self.width - filled), percent * 100), end = "")
-    
-    def erase(self):
-        print("\r" + " " * (self.width + 10), end = "\r")
-        
-    def finish(self):
-        print("\r[%s] %d%%" % ("#" * self.width, 100))
-        
-    def print(self, *values, sep = " ", end = "\n"):
-        self.erase()
-        print(*values, sep = sep, end = end)
-        self.draw()
-        
+
     
     
 def exists(obj, parent_folder):
---
+++ building/progressBar.py
@@ -0,0 +1,31 @@
+class ProgressBar:
+    def __init__(self, total = 100, width = 50):
+        self.total = total
+        self.current = 0
+        self.last = 0
+        self.width = width
+        
+    def update(self, current):
+        self.current = current
+        if self.current > self.total:
+            self.current = self.total
+        if self.current != self.last:
+            self.last = self.current
+            self.draw()
+            
+    def draw(self):
+        percent = self.current / self.total
+        filled = int(percent * self.width)
+        print("\r[%s%s] %d%%" % ("#" * filled, " " * (self.width - filled), percent * 100), end = "")
+    
+    def erase(self):
+        print("\r" + " " * (self.width + 10), end = "\r")
+        
+    def finish(self):
+        print("\r[%s] %d%%" % ("#" * self.width, 100))
+        
+    def print(self, *values, sep = " ", end = "\n"):
+        self.erase()
+        print(*values, sep = sep, end = end)
+        self.draw()
+        
